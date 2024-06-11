544d749a09eef7e177e3b56d604953328e975e12
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:45:24 +0300

    libil2cpp: Unity v5.4.0b16
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.4/5.4.0b16/char-conversions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+extern const uint8_t CategoryData_v4 [71680];
+extern const uint8_t NumericData [12938];
+extern const double NumericDataValues [58];
+extern const uint16_t ToLowerDataLow [9424];
+extern const uint16_t ToLowerDataHigh [223];
+extern const uint16_t ToUpperDataLow [9450];
+extern const uint16_t ToUpperDataHigh [223];
