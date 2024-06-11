6bcd49a5270d6cbb75671cc022c6c1f10c09c5a4
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 16:20:01 +0300

    libil2cpp: Unity v2019.1.0b8
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_2019.1/2019.1.0b8/os/Posix/Image.cpp
@@ -0,0 +1,39 @@
+#include "il2cpp-config.h"
+
+#if IL2CPP_TARGET_JAVASCRIPT || IL2CPP_TARGET_LINUX || IL2CPP_TARGET_LUMIN && !IL2CPP_TINY_WITHOUT_DEBUGGER
+
+#if IL2CPP_TARGET_JAVASCRIPT
+#include <emscripten/emscripten.h>
+#else
+#include <dlfcn.h>
+#endif
+
+namespace il2cpp
+{
+namespace os
+{
+namespace Image
+{
+    void Initialize()
+    {
+    }
+
+    void* GetImageBase()
+    {
+#if IL2CPP_TARGET_JAVASCRIPT
+        emscripten_log(EM_LOG_NO_PATHS | EM_LOG_CONSOLE | EM_LOG_ERROR | EM_LOG_JS_STACK, "Warning: libil2cpp/os/Posix/Image.cpp: GetImageBase() called, but dynamic libraries are not available.");
+        return NULL;
+#else
+        Dl_info info;
+        void* const anySymbol = reinterpret_cast<void*>(&GetImageBase);
+        if (dladdr(anySymbol, &info))
+            return info.dli_fbase;
+        else
+            return NULL;
+#endif
+    }
+}
+}
+}
+
+#endif
