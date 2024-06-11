6e2c455d78d36ad6a1aa4f75305095fa33c79f93
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:49:33 +0300

    libil2cpp: Unity v5.5.3f1
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.5/5.5.3f1/utils/PathUtils.cpp
@@ -0,0 +1,63 @@
+#include "il2cpp-config.h"
+#include "utils/PathUtils.h"
+#include <string>
+
+namespace il2cpp
+{
+namespace utils
+{
+namespace PathUtils
+{
+    std::string Basename(const std::string& path)
+    {
+        if (path.empty())
+            return ".";
+
+        const size_t pos = path.rfind(IL2CPP_DIR_SEPARATOR);
+
+        // No seperators. Path is filename
+        if (pos == std::string::npos)
+            return path;
+
+        return path.substr(pos + 1);
+    }
+
+    std::string BasenameNoExtension(const std::string& path)
+    {
+        if (path.empty())
+            return ".";
+
+        std::string base = Basename(path);
+
+        const size_t pos = base.rfind('.');
+
+        // No extension.
+        if (pos == std::string::npos)
+            return base;
+
+        return base.substr(0, pos);
+    }
+
+    std::string DirectoryName(const std::string& path)
+    {
+        if (path.empty())
+            return std::string();
+
+        const size_t pos = path.rfind(IL2CPP_DIR_SEPARATOR);
+
+        if (pos == std::string::npos)
+            return ".";
+
+        if (pos == 0)
+            return "/";
+
+        return path.substr(0, pos);
+    }
+
+    std::string Combine(const std::string& path1, const std::string& path2)
+    {
+        return path1 + IL2CPP_DIR_SEPARATOR + path2;
+    }
+}
+} /* utils */
+} /* il2cpp */
