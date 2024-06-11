47152cc38a3947c269d6d9ccb40693ebdb40ee5e
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:47:49 +0300

    libil2cpp: Unity v5.5.0b2
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.5/5.5.0b2/icalls/mscorlib/System/CLRConfig.cpp
@@ -0,0 +1,26 @@
+#if NET_4_0
+#include "il2cpp-config.h"
+#include "CLRConfig.h"
+
+namespace il2cpp
+{
+namespace icalls
+{
+namespace mscorlib
+{
+namespace System
+{
+
+bool CLRConfig::CheckThrowUnobservedTaskExceptions()
+{
+	NOT_IMPLEMENTED_ICALL(CLRConfig::CheckThrowUnobservedTaskExceptions);
+	IL2CPP_UNREACHABLE;
+	return NULL;
+}
+
+
+} // namespace System
+} // namespace mscorlib
+} // namespace icalls
+} // namespace il2cpp
+#endif
---
+++ libil2cpp/Unity_5.5/5.5.0b2/metadata/Il2CppSignatureCompare.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "utils/dynamic_array.h"
+
+struct Il2CppType;
+
+namespace il2cpp
+{
+namespace metadata
+{
+
+struct Il2CppSignatureCompare
+{
+	bool operator() (const KeyWrapper<dynamic_array<const Il2CppType*> >& s1, const KeyWrapper<dynamic_array<const Il2CppType*> >& s2) const;
+	static bool Equals (const KeyWrapper<dynamic_array<const Il2CppType*> >& s1, const KeyWrapper<dynamic_array<const Il2CppType*> >& s2);
+};
+
+} /* namespace metadata */
+} /* namespace il2cpp */
