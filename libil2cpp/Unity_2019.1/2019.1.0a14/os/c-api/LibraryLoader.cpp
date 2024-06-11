833990ff9500945a3bbd7da3dbf31387246b0c84
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 16:18:59 +0300

    libil2cpp: Unity v2019.1.0a14
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_2019.1/2019.1.0a14/os/c-api/LibraryLoader.cpp
@@ -0,0 +1,34 @@
+#include "os/c-api/il2cpp-config-platforms.h"
+
+#if !UNITY_TINY_WITHOUT_DEBUGGER
+
+#include "os/c-api/LibraryLoader-c-api.h"
+#include "os/LibraryLoader.h"
+#include "utils/StringUtils.h"
+
+extern "C"
+{
+    void* UnityPalLibraryLoaderLoadDynamicLibrary(const char* nativeDynamicLibrary, int flags)
+    {
+        Il2CppNativeString libName(il2cpp::utils::StringUtils::Utf8ToNativeString(nativeDynamicLibrary));
+        return il2cpp::os::LibraryLoader::LoadDynamicLibrary(libName, flags);
+    }
+
+    void UnityPalLibraryLoaderCleanupLoadedLibraries()
+    {
+        il2cpp::os::LibraryLoader::CleanupLoadedLibraries();
+    }
+
+    UnityPalMethodPointer UnityPalLibraryLoaderGetFunctionPointer(void* dynamicLibrary, const char* functionName)
+    {
+        return il2cpp::os::LibraryLoader::GetFunctionPointer(dynamicLibrary, functionName);
+    }
+
+    int32_t UnityPalLibraryLoaderCloseLoadedLibrary(void** dynamicLibrary)
+    {
+        IL2CPP_ASSERT(*dynamicLibrary);
+        return il2cpp::os::LibraryLoader::CloseLoadedLibrary(*dynamicLibrary);
+    }
+}
+
+#endif
