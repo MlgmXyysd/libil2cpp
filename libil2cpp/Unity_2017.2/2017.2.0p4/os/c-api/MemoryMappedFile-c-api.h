82d1384818f12d29b33e532ec65e4c17851a0a84
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:57:38 +0300

    libil2cpp: Unity v2017.2.0p4
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_2017.2/2017.2.0p4/os/c-api/MemoryMappedFile-c-api.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "os/c-api/File-c-api.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+void* UnityPalMemoryMappedFileMap(UnityPalFileHandle* file);
+void UnityPalMemoryMappedFileUnmap(void* address);
+
+void* UnityPalMemoryMappedFileMapWithParams(UnityPalFileHandle* file, size_t length, size_t offset);
+void UnityPalMemoryMappedFileUnmapWithParams(void* address, size_t length);
+
+#if defined(__cplusplus)
+}
+#endif
