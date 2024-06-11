56ea4853983213458c1fe0879c36fcb43c7267f3
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:42:15 +0300

    libil2cpp: Unity v5.2.3p3
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.2/5.2.3p3/os/Posix/Image.cpp
@@ -0,0 +1,23 @@
+#include "il2cpp-config.h"
+
+#if IL2CPP_TARGET_JAVASCRIPT || IL2CPP_TARGET_LINUX || IL2CPP_TARGET_ANDROID
+
+
+namespace il2cpp
+{
+namespace os
+{
+
+namespace Image
+{
+
+void* GetImageBase()
+{
+	return NULL;
+}
+
+}
+}
+}
+
+#endif
