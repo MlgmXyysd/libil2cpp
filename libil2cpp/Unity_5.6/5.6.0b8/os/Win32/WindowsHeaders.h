acda753d2100e7e4885c8461ea3adb5f0ef676cc
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:51:06 +0300

    libil2cpp: Unity v5.6.0b8
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.6/5.6.0b8/os/Win32/WindowsHeaders.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#if IL2CPP_TARGET_WINDOWS
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN 1
+#define INC_OLE2 1
+#include <Windows.h>
+
+#if !IL2CPP_TARGET_WINDOWS_DESKTOP
+#include <bcrypt.h>
+#include <roapi.h>
+#include <winstring.h>
+#endif
+
+#define LINK_TO_WINDOWSRUNTIME_LIBS (!IL2CPP_TARGET_WINDOWS_DESKTOP)
+
+#endif
