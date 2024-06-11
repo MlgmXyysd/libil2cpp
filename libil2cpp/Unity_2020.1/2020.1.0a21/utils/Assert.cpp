7d89c4faba51b18fd644fde3e5d9dba990d5039b
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 16:45:13 +0300

    libil2cpp: Unity v2020.1.0a21
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_2020.1/2020.1.0a21/utils/Assert.cpp
@@ -0,0 +1,29 @@
+#include "Assert.h"
+
+#if IL2CPP_DEBUG
+
+#if IL2CPP_TARGET_WINDOWS || IL2CPP_TARGET_XBOXONE || IL2CPP_TARGET_WINRT
+#include <crtdbg.h>
+#include <string>
+#else
+#include <cstdio>
+#include <cstdlib>
+#endif
+
+void il2cpp_assert(const char* assertion, const char* file, unsigned int line)
+{
+#if IL2CPP_TARGET_WINDOWS || IL2CPP_TARGET_XBOXONE || IL2CPP_TARGET_WINRT
+    std::string message = "Assertion failed: ";
+    message += assertion;
+    message += " file ";
+    message += file;
+    message += " line ";
+    message += std::to_string(line);
+    _ASSERT_EXPR(0, message.c_str());
+#else
+    printf("Assertion failed: %s, file %s, line %u", assertion, file, line);
+    abort();
+#endif
+}
+
+#endif
