b07a105256022a99c4c8cd3e5ad389d70bd327e5
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:54:08 +0300

    libil2cpp: Unity v2017.1.0b2
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_2017.1/2017.1.0b2/utils/Runtime.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace il2cpp
+{
+namespace utils
+{
+    class LIBIL2CPP_CODEGEN_API Runtime
+    {
+    public:
+        static NORETURN void Abort();
+    };
+} // utils
+} // il2cpp
