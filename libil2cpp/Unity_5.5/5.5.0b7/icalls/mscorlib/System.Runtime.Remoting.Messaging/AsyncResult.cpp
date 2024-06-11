d50cbf0beeaf6cbf9ee0ceff735e9be9a2133a5f
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:48:07 +0300

    libil2cpp: Unity v5.5.0b7
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.5/5.5.0b7/icalls/mscorlib/System.Runtime.Remoting.Messaging/AsyncResult.cpp
@@ -0,0 +1,35 @@
+#if NET_4_0
+#include "il2cpp-config.h"
+#include "AsyncResult.h"
+
+namespace il2cpp
+{
+namespace icalls
+{
+namespace mscorlib
+{
+namespace System
+{
+namespace Runtime
+{
+namespace Remoting
+{
+namespace Messaging
+{
+
+Il2CppObject* AsyncResult::Invoke(Il2CppObject* _this)
+{
+	NOT_IMPLEMENTED_ICALL(AsyncResult::Invoke);
+	IL2CPP_UNREACHABLE;
+	return NULL;
+}
+
+
+} // namespace Messaging
+} // namespace Remoting
+} // namespace Runtime
+} // namespace System
+} // namespace mscorlib
+} // namespace icalls
+} // namespace il2cpp
+#endif
