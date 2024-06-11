1903832c40ebfd3d4ba888cceb63855a71f55838
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:41:55 +0300

    libil2cpp: Unity v5.2.1p3
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.2/5.2.1p3/os/WinRT/Locale.cpp
@@ -0,0 +1,21 @@
+#include "il2cpp-config.h"
+
+#if IL2CPP_TARGET_WINRT
+
+#include "os/Locale.h"
+
+namespace il2cpp
+{
+namespace os
+{
+
+std::string Locale::GetLocale()
+{
+	NOT_IMPLEMENTED_NO_ASSERT(Locale::GetLocale, "TODO: implement properly");
+	return "en-US";
+}
+
+} /* namespace os */
+} /* namespace il2cpp */
+
+#endif
