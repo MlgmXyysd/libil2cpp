e850f0ecc4c0dff5217093938f7a3d0cae3d6064
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:43:41 +0300

    libil2cpp: Unity v5.3.5p2
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.3/5.3.5p2/vm/RCW.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "il2cpp-config.h"
+
+struct Il2CppComObject;
+struct Il2CppGuid;
+struct Il2CppIUnknown;
+
+namespace il2cpp
+{
+namespace vm
+{
+
+class LIBIL2CPP_CODEGEN_API RCW
+{
+public:
+	static void Initialize(Il2CppComObject* rcw, const Il2CppGuid& clsid);
+	static Il2CppIUnknown* QueryInterface(Il2CppComObject* rcw, const Il2CppGuid& iid, bool throwOnError);
+	static Il2CppObject* Create(Il2CppIUnknown* unknown);
+
+private:
+	static void Cleanup(void* obj, void* data);
+};
+
+} /* namespace vm */
+} /* namespace il2cpp */
