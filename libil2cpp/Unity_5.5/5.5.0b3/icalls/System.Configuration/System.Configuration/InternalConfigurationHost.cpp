18035a2531766b5f35fb5db68434a88bcf6d0e8e
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:47:53 +0300

    libil2cpp: Unity v5.5.0b3
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.5/5.5.0b3/icalls/System.Configuration/System.Configuration/InternalConfigurationHost.cpp
@@ -0,0 +1,32 @@
+#if NET_4_0
+#include "il2cpp-config.h"
+#include "InternalConfigurationHost.h"
+
+namespace il2cpp
+{
+namespace icalls
+{
+namespace System
+{
+namespace Configuration
+{
+namespace System
+{
+namespace Configuration
+{
+
+Il2CppString* InternalConfigurationHost::get_bundled_app_config()
+{
+	NOT_IMPLEMENTED_ICALL(InternalConfigurationHost::get_bundled_app_config);
+	IL2CPP_UNREACHABLE;
+	return NULL;
+}
+
+
+} // namespace Configuration
+} // namespace System
+} // namespace Configuration
+} // namespace System
+} // namespace icalls
+} // namespace il2cpp
+#endif
