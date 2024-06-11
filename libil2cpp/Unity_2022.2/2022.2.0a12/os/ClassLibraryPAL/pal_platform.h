db119498ed45e175d64a50fdc4c8af7e54418caf
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 17:59:35 +0300

    libil2cpp: Unity v2022.2.0a12
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_2022.2/2022.2.0a12/os/ClassLibraryPAL/pal_platform.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "il2cpp-config.h"
+
+#if IL2CPP_TARGET_LINUX
+#include "os/Linux/pal_linux.h"
+#elif IL2CPP_TARGET_QNX
+#include "os/QNX/pal_qnx.h"
+#elif IL2CPP_TARGET_DARWIN
+#include "os/OSX/pal_darwin.h"
+#elif IL2CPP_TARGET_ANDROID
+#include "os/Android/pal_android.h"
+#elif IL2CPP_TARGET_JAVASCRIPT
+#include "os/Emscripten/pal_emscripten.h"
+#elif IL2CPP_TARGET_SWITCH
+#include "pal/pal_switch.h"
+#elif IL2CPP_TARGET_PS4 || IL2CPP_TARGET_PS5
+#include "pal/pal_playstation.h"
+#endif
