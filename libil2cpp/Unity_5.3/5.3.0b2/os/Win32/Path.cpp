05780e4ab6bf6b9de9552a7e4abf466db52e15c8
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:42:23 +0300

    libil2cpp: Unity v5.3.0b2
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.3/5.3.0b2/os/Win32/Path.cpp
@@ -0,0 +1,36 @@
+#include "il2cpp-config.h"
+
+#if IL2CPP_TARGET_WINDOWS
+
+#include "WindowsHeaders.h"
+#undef GetTempPath
+
+#include "os/Environment.h"
+#include "os/Path.h"
+#include "utils/StringUtils.h"
+#include <string>
+
+namespace il2cpp
+{
+namespace os
+{
+
+std::string Path::GetExecutablePath()
+{
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileNameW(NULL, buffer, MAX_PATH);
+	return utils::StringUtils::Utf16ToUtf8(reinterpret_cast<const uint16_t*>(buffer));
+}
+
+std::string Path::GetTempPath()
+{
+	WCHAR tempPath[MAX_PATH + 1];
+	::GetTempPathW(sizeof(tempPath) / sizeof(tempPath[0]), tempPath);
+	::GetLongPathNameW(tempPath, tempPath, sizeof(tempPath) / sizeof(tempPath[0]));
+
+	return utils::StringUtils::Utf16ToUtf8((uint16_t*)tempPath);
+}
+
+}
+}
+#endif
