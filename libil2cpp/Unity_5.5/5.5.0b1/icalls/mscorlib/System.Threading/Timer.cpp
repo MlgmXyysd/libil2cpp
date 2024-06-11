1f76b9417a918e629a0deb35190ade8e6d899125
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:47:45 +0300

    libil2cpp: Unity v5.5.0b1
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.5/5.5.0b1/icalls/mscorlib/System.Threading/Timer.cpp
@@ -0,0 +1,28 @@
+#if NET_4_0
+#include "il2cpp-config.h"
+#include "Timer.h"
+
+namespace il2cpp
+{
+namespace icalls
+{
+namespace mscorlib
+{
+namespace System
+{
+namespace Threading
+{
+
+int64_t Timer::GetTimeMonotonic()
+{
+	NOT_IMPLEMENTED_ICALL(Timer::GetTimeMonotonic);
+	IL2CPP_UNREACHABLE;
+}
+
+
+} // namespace Threading
+} // namespace System
+} // namespace mscorlib
+} // namespace icalls
+} // namespace il2cpp
+#endif
