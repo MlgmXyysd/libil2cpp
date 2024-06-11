9eac928f5f325aad246550a67862cce88b3d13f4
Jaida Wu <mlgmxyysd@meowcat.org>
2024-06-11 15:45:10 +0300

    libil2cpp: Unity v5.4.0b11
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>

---
+++ libil2cpp/Unity_5.4/5.4.0b11/icalls/System/System.Diagnostics/PerformanceCounterCategory.cpp
@@ -0,0 +1,80 @@
+#include "il2cpp-config.h"
+
+#include <cassert>
+
+#include "icalls/System/System.Diagnostics/PerformanceCounterCategory.h"
+#include "vm/Exception.h"
+
+namespace il2cpp
+{
+namespace icalls
+{
+namespace System
+{
+namespace System
+{
+namespace Diagnostics
+{
+
+
+bool PerformanceCounterCategory::CategoryDelete (Il2CppString* name)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::CategoryDelete);
+	
+	return false;
+}
+
+Il2CppString* PerformanceCounterCategory::CategoryHelpInternal (Il2CppString* category, Il2CppString* machine)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::CategoryHelpInternal);
+	
+	return 0;
+}
+
+bool PerformanceCounterCategory::CounterCategoryExists (Il2CppString* counter, Il2CppString* category, Il2CppString* machine)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::CounterCategoryExists);
+	
+	return false;
+}
+
+bool PerformanceCounterCategory::Create (Il2CppString* categoryName, Il2CppString* categoryHelp, PerformanceCounterCategoryType categoryType, Il2CppArray* items)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::Create);
+	
+	return false;
+}
+
+int32_t PerformanceCounterCategory::InstanceExistsInternal (Il2CppString* instance, Il2CppString* category, Il2CppString* machine)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::InstanceExistsInternal);
+	
+	return 0;
+}
+
+Il2CppArray* PerformanceCounterCategory::GetCategoryNames (Il2CppString* machine)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::GetCategoryNames);
+	
+	return 0;
+}
+
+Il2CppArray* PerformanceCounterCategory::GetCounterNames (Il2CppString* category, Il2CppString* machine)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::GetCounterNames);
+	
+	return 0;
+}
+
+Il2CppArray* PerformanceCounterCategory::GetInstanceNames (Il2CppString* category, Il2CppString* machine)
+{
+	NOT_IMPLEMENTED_ICALL (PerformanceCounterCategory::GetInstanceNames);
+	
+	return 0;
+}
+
+} /* namespace Diagnostics */
+} /* namespace System */
+} /* namespace System */
+} /* namespace icalls */
+} /* namespace il2cpp */
